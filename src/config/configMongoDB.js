5ef963ae7e7ee40c8859e36548d396fc4219bfe7
Javier Simon <simonjavier.fullstack@gmail.com>
2023-06-05 21:53:59 -0300

    desafio 6

---
+++ src/config/configMongoDB.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const config = {
+  mongoDB: {
+    URL: "mongodb+srv://javisimon22:dJrUwMt8jA9kgApw@data-base.shzhzce.mongodb.net/?retryWrites=true&w=majority",
+      options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+};
+
+export const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(config.mongoDB.URL, config.mongoDB.options);
+    console.log("Connected to Mongo Atlas");
+  } catch (error) {
+    console.log("Error en la conexión con Mongo Atlas", error);
+  }
+};
